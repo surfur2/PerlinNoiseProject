fcffb56b7958893b7de95d5968a26aee9bbd2bc0
WAVEBREAKER\NateBlair <nategribble@gmail.com>
2017-11-15 13:35:31 -0500

    Generate basic mountains in the middle of my terrain.

--- PerlinNoiseScene/Assets/Scripts/TerrainPerlinNoise.cs
+++ PerlinNoiseScene/Assets/Scripts/TerrainPerlinNoise.cs
@@ -1,11 +1,18 @@
 ﻿using UnityEditor;
 using UnityEngine;
 using System.Collections;
+using CustomPerlinNoise;
 
 public class TerrainPerlinNoise : MonoBehaviour
 {
 
     public float Tiling = 10.0f;
+    public int octaves;
+    public float maximumHeight;
+    public float heightOfFields;
+    public float heightForMountains;
+    public int mountainsCenter;
+    public float mountainRadius;
 
     private PerlinNoise myPerlinNoiseGenerator;
 
@@ -25,14 +32,32 @@ public class TerrainPerlinNoise : MonoBehaviour
     public void GenerateHeights(Terrain terrain, float tileSize)
     {
         float[,] heights = new float[terrain.terrainData.heightmapWidth, terrain.terrainData.heightmapHeight];
+        TerrainAreaInfo fieldsInfo = new TerrainAreaInfo(terrain.terrainData.heightmapWidth, terrain.terrainData.heightmapHeight, octaves, 0, terrain.terrainData.heightmapWidth);
+        TerrainAreaInfo mountainInfo = new TerrainAreaInfo(terrain.terrainData.heightmapWidth, terrain.terrainData.heightmapHeight, octaves, 0, terrain.terrainData.heightmapWidth);
 
-        var perlinHeights = myPerlinNoiseGenerator.GenerateHeightMap(terrain.terrainData.heightmapWidth, terrain.terrainData.heightmapHeight, 6);
+        var perlinHeights = myPerlinNoiseGenerator.GenerateHeightMap(fieldsInfo);
+
+        var maximumMountianHeight = (heightForMountains / maximumHeight);
+        var maximumFieledHeight = (heightOfFields / maximumHeight);
 
         for (int i = 0; i < terrain.terrainData.size.x; i++)
         {
             for (int k = 0; k < terrain.terrainData.size.z; k++)
             {
-                heights[i, k] = perlinHeights[i][k];
+                var distanceToMountains = Mathf.Sqrt((mountainsCenter - i) * (mountainsCenter - i) + (mountainsCenter - k) * (mountainsCenter - k));
+
+                // Mountainas areas
+                if (distanceToMountains <= mountainRadius)
+                {
+                    var multiplierToMaxHeight = (float)(mountainRadius - distanceToMountains) / (float)mountainRadius;
+                    var nextHeight = Mathf.Max(perlinHeights[i][k] * maximumMountianHeight * multiplierToMaxHeight, perlinHeights[i][k] * maximumFieledHeight);
+                    heights[i, k] = nextHeight;
+                }
+                // Field area
+                else
+                {
+                    heights[i, k] = perlinHeights[i][k] * maximumFieledHeight;
+                }
             }
         }
 
